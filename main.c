b2b77cc75e9650a79690c471f2b3c0f5f5983397
Otavio Fagundes <otaviofariafagundes1@gmail.com>
2024-08-18 22:21:01 -0300

    entrada e saida de veiculos

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <Windows.h>
 
 void menu(){
     int opcao;
@@ -17,25 +18,133 @@ void menu(){
         switch (opcao)
         {
         case 1:
-            cadastrarVeiculo();
+            opcao1();
             break;
         case 2:
-            printf("entrou no 2 \n\n");
+            opcao2();
             break;
         case 3:
             printf("entrou no 3 \n\n");
             break;
         case 4:
-            printf("-------------------------------------------\n\n");
+            opcao4();
             break;
         case 5:
             break;
         default:
             printf("Opcao invalida!");
-            // getch();
             system("cls");
         }
     } while (opcao != 5);
+}
+
+void opcao1(){
+	int i = 1;
+	
+	if(i == 10){
+		printf("Estacionamento esta cheio");
+		return 1;
+	}else{
+		entradaVeiculo();
+		printf("Veiculo ja pode estacionar");
+		Sleep(2000);
+		system("cls");
+		i++;
+		return 1;
+	}	
+}
+
+void entradaVeiculo() {
+    char placa[8];
+    char motorista[30];
+    char marca[30];
+    char modelo[30];
+    char horarioEntrada[6];
+    int flag;
+    
+    do {
+        flag = 0;
+        printf("Informe a placa: ");
+        scanf("%s", placa);
+        
+        flag = conferePlaca(placa);
+        
+    } while (flag == 1);
+    
+    printf("Informe o nome do motorista: ");
+    scanf("%s", motorista);
+    
+    printf("Informe a marca do carro: ");
+    scanf("%s", marca);
+    
+    printf("Informe o modelo do carro: ");
+    scanf("%s", modelo);
+    
+    printf("Informe o horario de entrada do carro: ");
+    scanf("%s", horarioEntrada);
+    
+}
+
+int conferePlaca(char placa[]) {
+    int flag = 0;
+    int tamanhoPlaca = strlen(placa);
+    
+    if (tamanhoPlaca != 7) {
+        if (tamanhoPlaca < 7) {
+            printf("Placa invalida\n Digitos insuficientes\n\n");
+        } else {
+            printf("Placa invalida\n Muitos digitos\n\n");
+        }
+        flag = 1;
+    }
+    
+    for (int i = 0; i < 7; i++) {
+        placa[i] = toupper(placa[i]);  
+    }
+
+    return flag;
+}
+
+void opcao2(){
+	char placa[8];
+	char horarioSaida[6];
+	int flag, i;
+	int valorTotal = 0;
+	
+	do {
+        flag = 0;
+        printf("Informe a placa: ");
+        scanf("%s", placa);
+        
+        flag = conferePlaca(placa);
+        
+    } while (flag == 1);
+	
+	if(placa == placa){
+		printf("Informe o horario de saida do carro: ");
+    	scanf("%s", horarioSaida);
+		
+		//valorTotal = (horarioSaida - horarioEntrada)*valorHora;
+		
+		printf("veiculo ja pode sair");
+		Sleep(2000);
+		system("cls");
+		i--;
+	}else{
+		printf("Veiculo nao esta no estacionamento");
+	}
+}
+
+void opcao4(){
+	float valor;
+	
+	printf("valor atual e %.2f\n", valor);
+	printf("Qual sera o novo valor? ");
+	scanf("%f", &valor);
+	
+	printf("Valor atualizado para %.2f", valor);
+	Sleep(2000);
+	system("cls");
 }
 
 main(){
